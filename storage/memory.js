d566d1811d19a33b9cbf515166b311f2163a9dc9
markb-trustifi <mbabayev@trustificorp.com>
2020-10-29 14:30:30 +0200

    added logger

--- storage/memory.js
+++ storage/memory.js
@@ -1,14 +1,26 @@
-var concat = require('concat-stream')
+// var concat = require('concat-stream')
 
 function MemoryStorage (opts) {}
 
 MemoryStorage.prototype._handleFile = function _handleFile (req, file, cb) {
-  file.stream.pipe(concat({ encoding: 'buffer' }, function (data) {
+  /* file.stream.pipe(concat({ encoding: 'buffer' }, function (data) {
     cb(null, {
       buffer: data,
       size: data.length
     })
-  }))
+  })) */
+
+  var chunks = []
+  var error = null
+  file.stream.on('data', chunk => chunks.push(chunk))
+  file.stream.on('error', err => { error = err })
+  file.stream.on('end', () => {
+    chunks = Buffer.concat(chunks)
+    cb(error, {
+      buffer: chunks,
+      size: chunks.length
+    })
+  })
 }
 
 MemoryStorage.prototype._removeFile = function _removeFile (req, file, cb) {
